[fix] zero out gradients

TA_Trainer.py
@@ -42,7 +42,8 @@ class TATrainer(Trainer):
             diff = sum(diff)
             if diff >= self.thrd:
                 timed_log(f'{self.name} reporting delta')
-                for layer in enumerate(model_fresh.parameters()):
+                for layer, p in enumerate(model_fresh.parameters()):
+                    p.grad.zero_()
                     v = self.momentum_dict[f'weight_v_{layer}'] = self.beta_2 * \
                         self.momentum_dict[f'weight_v_{layer}'] + \
                         (1 - self.beta_2)*torch.pow(grad[layer], 2)