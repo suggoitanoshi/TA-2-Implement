[fix] diff calculation and sending

TA_PS.py
@@ -25,7 +25,7 @@ class TAParameterServer(BatchUpdateParameterServer):
             e.add_(self.delta_hat[i] - delta_tilde[i])
         for i, p in enumerate(self.model.to(self.device).parameters()):
             p.add_(-delta_tilde[i])
-        diff = sum([torch.norm(d, 2) for d in delta_tilde])
+        diff = torch.sqrt(sum([d.pow(2) for d in delta_tilde]))
         self.triggerlist.append(diff)
         self.triggerlist.pop(0)
         thrd = sum(self.triggerlist)*self.thrd_scale
@@ -33,7 +33,7 @@ class TAParameterServer(BatchUpdateParameterServer):
         self.add_bits_curr_epoch(
             sum([delta.nelement() * delta.element_size() for delta in delta_tilde]))
         self.add_bits_curr_epoch(64)
-        fut.set_result({"delta_tilde": delta_tilde, "thrd": thrd})
+        fut.set_result({"delta_tilde": delta_tilde, "thrd": thrd.item()})
 
     def _update_model(self, worker, data):
         fut = self.future_model