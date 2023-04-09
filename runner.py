5d207f01e59d9e7b4cd24063a184fe12e3573dfa
Fransiskus Febryan Suryawan <franfebryan@protonmail.com>
2023-04-09 20:00:47 +0700

    [fix] method to set defaults for addr and port

--- runner.py
+++ runner.py
@@ -56,10 +56,10 @@ def run_ps(trainers, PS, PS_args, Trainer, Trainer_args, stats_running_file, che
 
 
 def run(rank, world_size, PS, PS_args, Trainer, Trainer_args, stats_running_file, checkpoint_file, args):
-    MASTER_ADDR = os.environ['MASTER_ADDR']
-    MASTER_PORT = os.environ['MASTER_PORT']
-    os.environ['MASTER_ADDR'] = MASTER_ADDR if MASTER_ADDR != '' else 'localhost'
-    os.environ['MASTER_PORT'] = MASTER_PORT if MASTER_PORT != '' else portpicker.pick_unused_port()
+    MASTER_ADDR = os.environ.get('MASTER_ADDR', 'localhost')
+    MASTER_PORT = os.environ.get('MASTER_PORT', portpicker.pick_unused_port())
+    os.environ['MASTER_ADDR'] = MASTER_ADDR
+    os.environ['MASTER_PORT'] = MASTER_PORT
     options = rpc.TensorPipeRpcBackendOptions(
         num_worker_threads=4,
         rpc_timeout=0  # infinite timeout
